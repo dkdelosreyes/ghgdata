46208a8c126aeb295367a3ffd8d78d980e144c8a
Dianne Delos Reyes <dianne.delosreyes@talkpush.com>
2024-07-01 04:54:11 +0800

    removed 'test' folder. init rspec

--- test/models/data_group_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class DataGroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/facility_summary_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class FacilitySummaryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
